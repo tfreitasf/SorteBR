d98ab1cb09881cdd3b5be50acadd689daad4b5b5
tfreitasf <tfreitasf@gmail.com>
2023-09-13 17:55:04 -0300

    refactor: Refatorar lógica de geração de números
    - Melhorar a clareza e organização do código.

--- app/src/main/java/br/com/povengenharia/sortebr/MainActivity.kt
+++ app/src/main/java/br/com/povengenharia/sortebr/MainActivity.kt
@@ -2,7 +2,6 @@ package br.com.povengenharia.sortebr
 
 import android.app.Activity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -32,33 +31,28 @@ class MainActivity : Activity() {
     }
 
     private fun numberGenerator(text: String, txtResult: TextView) {
-        if (text.isNotEmpty()) {
 
-            val qtd = text.toInt()
-
-            if (qtd >= 6 && qtd <= 15) {
-
-                val numbers = mutableSetOf<Int>()
-                val random = Random()
-
-                while (true) {
-                    val number = random.nextInt(60)
-                    numbers.add(number + 1)
-
-                    if (numbers.size == qtd) {
-                        break
-                    }
-                }
-                txtResult.text = numbers.joinToString("-")
+        val qtd = text.toIntOrNull()
 
 
-            } else {
-                Toast.makeText(this, "Informe um número entre 6 e 15", Toast.LENGTH_LONG).show()
-            }
-
-        } else {
+        if (text.isEmpty() || qtd == null || qtd < 6 || qtd > 15) {
             Toast.makeText(this, "Informe um número entre 6 e 15", Toast.LENGTH_LONG).show()
+            return
         }
 
+        val numbers = mutableSetOf<Int>()
+        val random = Random()
+
+        while (true) {
+            val number = random.nextInt(60)
+            numbers.add(number + 1)
+
+            if (numbers.size == qtd) {
+                break
+            }
+        }
+        txtResult.text = numbers.sorted().joinToString("-")
     }
+
+
 }
